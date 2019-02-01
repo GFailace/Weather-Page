b5b4591edf63a6e386e945145c383fb12a0fa71c
Guilherme Failace <guilherme.failace@gmail.com>
2019-02-01 16:32:40 -0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,44 +1,58 @@
-/* var ctx = document.getElementById("myChart");
-var ctx = document.getElementById("myChart").getContext("2d");
-var ctx = $("#myChart");
-var ctx = "myChart";
+$.getJSON("http://dataservice.accuweather.com/forecasts/v1/daily/5day/45561?apikey=fqmF6AAGR8p6owNBkXMhD2AAAF5nwrvO&language=pt-br&details=true&metric=true", function (data) {
+    console.log(data)
 
-new Chart(ctx, {
-    data: {
-        datasets: [
-            {fill: 'origin'},      // 0: fill to 'origin'
-            {fill: '+2'},          // 1: fill to dataset 3
-            {fill: 1},             // 2: fill to dataset 1
-            {fill: false},         // 3: no fill
-            {fill: '-2'}           // 4: fill to dataset 2
-        ]
+    if (data.DailyForecasts[0].Day.Icon > 9) {
+        $('#icon0').attr('src', `https://developer.accuweather.com/sites/default/files/${data.DailyForecasts[0].Day.Icon}-s.png`)
     }
-}) */
+    else {
+        $('#icon0').attr('src', `https://developer.accuweather.com/sites/default/files/0${data.DailyForecasts[0].Day.Icon}-s.png`)
+    }
+    $('#weather0').append(data.DailyForecasts[0].Day.IconPhrase)
+    $('#min-max0').append("MIN - " + Math.floor(data.DailyForecasts[0].Temperature.Minimum.Value) + "º - " + Math.floor(data.DailyForecasts[0].Temperature.Maximum.Value) + "º - MAX")
 
-$.getJSON("http://api.openweathermap.org/data/2.5/forecast?id=3452925&units=metric&APPID=c91045cab5ae5847dbce5a2bdfb90cc4", function(data){
-console.log(data)
+    if (data.DailyForecasts[1].Day.Icon > 9) {
+        $('#icon1').attr('src', `https://developer.accuweather.com/sites/default/files/${data.DailyForecasts[1].Day.Icon}-s.png`)
+    }
+    else {
+        $('#icon1').attr('src', `https://developer.accuweather.com/sites/default/files/0${data.DailyForecasts[1].Day.Icon}-s.png`)
+    }
+    $('#weather1').append(data.DailyForecasts[1].Day.IconPhrase)
+    $('#min-max1').append("MIN - " + Math.floor(data.DailyForecasts[1].Temperature.Minimum.Value) + "º - " + Math.floor(data.DailyForecasts[1].Temperature.Maximum.Value) + "º - MAX")
 
-$('#city').append(`${data.city.name}, previsão para os próximos cinco dias`)
+    if (data.DailyForecasts[2].Day.Icon > 9) {
+        $('#icon2').attr('src', `https://developer.accuweather.com/sites/default/files/${data.DailyForecasts[2].Day.Icon}-s.png`)
+    }
+    else {
+        $('#icon2').attr('src', `https://developer.accuweather.com/sites/default/files/0${data.DailyForecasts[2].Day.Icon}-s.png`)
+    }
+    $('#weather2').append(data.DailyForecasts[2].Day.IconPhrase)
+    $('#min-max2').append("MIN - " + Math.floor(data.DailyForecasts[2].Temperature.Minimum.Value) + "º - " + Math.floor(data.DailyForecasts[2].Temperature.Maximum.Value) + "º - MAX")
 
-$('#icon0').attr('src', `http://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png`)
-$('#temp0').append(Math.floor(data.list[0].main.temp) + "ºC")
-$('#weather0').append(data.list[0].weather[0].main)
+    if (data.DailyForecasts[3].Day.Icon > 9) {
+        $('#icon3').attr('src', `https://developer.accuweather.com/sites/default/files/${data.DailyForecasts[3].Day.Icon}-s.png`)
+    }
+    else {
+        $('#icon3').attr('src', `https://developer.accuweather.com/sites/default/files/0${data.DailyForecasts[3].Day.Icon}-s.png`)
+    }
+    $('#weather3').append(data.DailyForecasts[3].Day.IconPhrase)
+    $('#min-max3').append("MIN - " + Math.floor(data.DailyForecasts[3].Temperature.Minimum.Value) + "º - " + Math.floor(data.DailyForecasts[3].Temperature.Maximum.Value) + "º - MAX")
 
-$('#icon1').attr('src', `http://openweathermap.org/img/w/${data.list[1].weather[0].icon}.png`)
-$('#temp1').append(Math.floor(data.list[1].main.temp) + "ºC")
-$('#weather1').append(data.list[1].weather[0].main)
+    if (data.DailyForecasts[4].Day.Icon > 9) {
+        $('#icon4').attr('src', `https://developer.accuweather.com/sites/default/files/${data.DailyForecasts[4].Day.Icon}-s.png`)
+    }
+    else {
+        $('#icon4').attr('src', `https://developer.accuweather.com/sites/default/files/0${data.DailyForecasts[4].Day.Icon}-s.png`)
+    }
+    $('#weather4').append(data.DailyForecasts[4].Day.IconPhrase)
+    $('#min-max4').append("MIN - " + Math.floor(data.DailyForecasts[4].Temperature.Minimum.Value) + "º - " + Math.floor(data.DailyForecasts[4].Temperature.Maximum.Value) + "º - MAX")
 
-$('#icon2').attr('src', `http://openweathermap.org/img/w/${data.list[2].weather[0].icon}.png`)
-$('#temp2').append(Math.floor(data.list[2].main.temp) + "ºC")
-$('#weather2').append(data.list[2].weather[0].main)
+})
 
-$('#icon3').attr('src', `http://openweathermap.org/img/w/${data.list[3].weather[0].icon}.png`)
-$('#temp3').append(Math.floor(data.list[3].main.temp) + "ºC")
-$('#weather3').append(data.list[3].weather[0].main)
-
-$('#icon4').attr('src', `http://openweathermap.org/img/w/${data.list[4].weather[0].icon}.png`)
-$('#temp4').append(Math.floor(data.list[4].main.temp) + "ºC")
-$('#weather4').append(data.list[4].weather[0].main)
-
+$.getJSON("http://dataservice.accuweather.com/currentconditions/v1/45561?apikey=fqmF6AAGR8p6owNBkXMhD2AAAF5nwrvO&language=pt-br&details=true&metric=true", function (result) {
+    console.log(result)
+    $('#current-icon').attr('src', `https://developer.accuweather.com/sites/default/files/0${result[0].WeatherIcon}-s.png`)
+    $('#current-weather').append(result[0].WeatherText)
+    $('#current-temp').append(Math.floor(result[0].Temperature.Metric.Value) + "ºC")
+    $('#current-min-max').append("MIN - " + Math.floor(result[0].TemperatureSummary.Past6HourRange.Minimum.Metric.Value) + "º - " + Math.floor(result[0].TemperatureSummary.Past6HourRange.Maximum.Metric.Value) + "º - MAX")
 })
 
